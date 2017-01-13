f1a7c6f861c46b228b8463c0fb52215387ad2e1f
sudomilk <mailertaylor@gmail.com>
2017-01-13 12:43:00 -0600

    stole generator that i liked from aisaacroth but modified it

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -2,13 +2,14 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    return tuple(open('dictionary.txt', 'r').read().splitlines())
+    with open('dictionary.txt', 'r') as f:
+        return f.read().splitlines()
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
     #words with hypens do not count in Scrabble
-    return sum([LETTER_SCORES[l.upper()] for l in word if l != '-'])
+    return sum(LETTER_SCORES[l.upper()] for l in word if l != '-')
 
 def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
